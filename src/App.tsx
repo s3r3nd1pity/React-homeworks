4c0c58c929fbcd8ad7f2fdc34e2503ef5fd06747
s3r3nd1pity <sasaserbinena@gmail.com>
2025-01-19 18:27:57 -0700

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 
 import './App.css'
-import {FamilyComponent} from "./components/family-component/FamilyComponent.tsx";
+
 
 function App() {
 
 
     return (
-        <>
-            <FamilyComponent></FamilyComponent>
-        </>
+       <>
+
+       </>
     )
 }
 
